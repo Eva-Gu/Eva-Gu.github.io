7c8360d0be1be37d56cf992108f916b62a00438b
Eva-Gu <55778852+Eva-Gu@users.noreply.github.com>
2019-11-26 01:12:12 +0800

    Update main.js

--- personal-website/assets/js/main.js
+++ personal-website/assets/js/main.js
@@ -1,4 +1,9 @@
+function onTopClick() {
+    window.location.hash = "#abc";
+}
 
-    function onTopClick() {
-         window.location.hash = "#abc";
-       }
+
+   
+    <input  type="button" name="Submit" value="提交"  onclick="javascript:onTopClick();" />
+
+    <div id="abc">跳轉到的位置</div>
