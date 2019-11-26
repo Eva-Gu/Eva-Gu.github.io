d45947cb3620c14b407f29cbdbfaf03c31266d54
Eva-Gu <55778852+Eva-Gu@users.noreply.github.com>
2019-11-26 20:31:09 +0800

    Update main.js

--- personal-website/assets/js/main.js
+++ personal-website/assets/js/main.js
@@ -1,9 +1,6 @@
-function onTopClick() {
-    window.location.hash = "#abc";
-}
+   /*<embed allowscriptaccess="never"  src="https://www.youtube.com/v/3rvVYZAAhIc&rel=1&autoplay=1" type="application/x-shockwave-flash" width="1" height="1"></embed> 
+    
+<embed hidden="true" src="assets/other/music.mp3"　width="70"　height="25"　autostart="true"　loop="true">
 
-
-   
-    <input  type="button" name="Submit" value="提交"  onclick="javascript:onTopClick();" />
-
-    <div id="abc">跳轉到的位置</div>
+    
+    
