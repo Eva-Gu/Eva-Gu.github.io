ae3acbf6d2a33f17baaba961e6539dd02eaa6d5c
Eva-Gu <55778852+Eva-Gu@users.noreply.github.com>
2019-11-27 20:24:16 +0800

    Delete main.js

--- personal-website/assets/js/main.js
+++
@@ -1,6 +0,0 @@
-   /*<embed allowscriptaccess="never"  src="https://www.youtube.com/v/3rvVYZAAhIc&rel=1&autoplay=1" type="application/x-shockwave-flash" width="1" height="1"></embed> 
-    
-<embed hidden="true" src="assets/other/music.mp3"　width="70"　height="25"　autostart="true"　loop="true">
-
-    
-    
