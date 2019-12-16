143e499c71f95d235e107fc6b51df7803ca45f5c
Eva-Gu <55778852+Eva-Gu@users.noreply.github.com>
2019-12-17 02:58:40 +0800

    Update commodity.js

--- terminal/assets/js/commodity.js
+++ terminal/assets/js/commodity.js
@@ -11,10 +11,10 @@ $(document).ready(function() {
 $(document).ready(function() {
 	// hides the slickbox as soon as the DOM is ready
 	// (a little sooner than page load)
-	$('#hiddenbox1').hide();
+	$('#hiddenboxc1').hide();
 	// toggles the slickbox on clicking the noted link
-	$('a#jqtextcall1').click(function() {
-		$('#hiddenbox1').slideToggle(400);
+	$('a#jqtextcallc1').click(function() {
+		$('#hiddenboxc1').slideToggle(400);
 		return false;
 	});
 });
