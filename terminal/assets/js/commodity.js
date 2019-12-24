aa2cfbbd64da1b7f380910ce39ce4dcdfd14621c
Eva-Gu <55778852+Eva-Gu@users.noreply.github.com>
2019-12-24 17:17:18 +0800

    Update commodity.js

--- terminal/assets/js/commodity.js
+++ terminal/assets/js/commodity.js
@@ -33,3 +33,208 @@ $(document).ready(function() {
 });
 
 /*改end*/
+
+/*change begin*/
+$(document).ready(function() {
+	// hides the slickbox as soon as the DOM is ready
+	// (a little sooner than page load)
+	$('#hiddenboxc3').hide();
+	// toggles the slickbox on clicking the noted link
+	$('a#jqtextcallc3').click(function() {
+		$('#hiddenboxc3').slideToggle(400);
+		return false;
+	});
+});
+$(document).ready(function() {
+	// hides the slickbox as soon as the DOM is ready
+	// (a little sooner than page load)
+	$('#hiddenboxc4').hide();
+	// toggles the slickbox on clicking the noted link
+	$('a#jqtextcallc4').click(function() {
+		$('#hiddenboxc4').slideToggle(400);
+		return false;
+	});
+});
+$(document).ready(function() {
+	// hides the slickbox as soon as the DOM is ready
+	// (a little sooner than page load)
+	$('#hiddenboxc5').hide();
+	// toggles the slickbox on clicking the noted link
+	$('a#jqtextcallc5').click(function() {
+		$('#hiddenboxc5').slideToggle(400);
+		return false;
+	});
+});
+$(document).ready(function() {
+	// hides the slickbox as soon as the DOM is ready
+	// (a little sooner than page load)
+	$('#hiddenboxc6').hide();
+	// toggles the slickbox on clicking the noted link
+	$('a#jqtextcallc6').click(function() {
+		$('#hiddenboxc6').slideToggle(400);
+		return false;
+	});
+});
+$(document).ready(function() {
+	// hides the slickbox as soon as the DOM is ready
+	// (a little sooner than page load)
+	$('#hiddenboxc7').hide();
+	// toggles the slickbox on clicking the noted link
+	$('a#jqtextcallc7').click(function() {
+		$('#hiddenboxc7').slideToggle(400);
+		return false;
+	});
+});
+
+$(document).ready(function() {
+	// hides the slickbox as soon as the DOM is ready
+	// (a little sooner than page load)
+	$('#hiddenboxb1').hide();
+	// toggles the slickbox on clicking the noted link
+	$('a#jqtextcallb1').click(function() {
+		$('#hiddenboxb1').slideToggle(400);
+		return false;
+	});
+});
+$(document).ready(function() {
+	// hides the slickbox as soon as the DOM is ready
+	// (a little sooner than page load)
+	$('#hiddenboxb2').hide();
+	// toggles the slickbox on clicking the noted link
+	$('a#jqtextcallb2').click(function() {
+		$('#hiddenboxb2').slideToggle(400);
+		return false;
+	});
+});
+$(document).ready(function() {
+	// hides the slickbox as soon as the DOM is ready
+	// (a little sooner than page load)
+	$('#hiddenboxb3').hide();
+	// toggles the slickbox on clicking the noted link
+	$('a#jqtextcallb3').click(function() {
+		$('#hiddenboxb3').slideToggle(400);
+		return false;
+	});
+});
+$(document).ready(function() {
+	// hides the slickbox as soon as the DOM is ready
+	// (a little sooner than page load)
+	$('#hiddenboxb4').hide();
+	// toggles the slickbox on clicking the noted link
+	$('a#jqtextcallb4').click(function() {
+		$('#hiddenboxb4').slideToggle(400);
+		return false;
+	});
+});
+$(document).ready(function() {
+	// hides the slickbox as soon as the DOM is ready
+	// (a little sooner than page load)
+	$('#hiddenboxb5').hide();
+	// toggles the slickbox on clicking the noted link
+	$('a#jqtextcallb5').click(function() {
+		$('#hiddenboxb5').slideToggle(400);
+		return false;
+	});
+});
+$(document).ready(function() {
+	// hides the slickbox as soon as the DOM is ready
+	// (a little sooner than page load)
+	$('#hiddenboxf1').hide();
+	// toggles the slickbox on clicking the noted link
+	$('a#jqtextcallf1').click(function() {
+		$('#hiddenboxf1').slideToggle(400);
+		return false;
+	});
+});
+$(document).ready(function() {
+	// hides the slickbox as soon as the DOM is ready
+	// (a little sooner than page load)
+	$('#hiddenboxf2').hide();
+	// toggles the slickbox on clicking the noted link
+	$('a#jqtextcallf2').click(function() {
+		$('#hiddenboxf2').slideToggle(400);
+		return false;
+	});
+});
+$(document).ready(function() {
+	// hides the slickbox as soon as the DOM is ready
+	// (a little sooner than page load)
+	$('#hiddenboxf3').hide();
+	// toggles the slickbox on clicking the noted link
+	$('a#jqtextcallf3').click(function() {
+		$('#hiddenboxf3').slideToggle(400);
+		return false;
+	});
+});
+$(document).ready(function() {
+	// hides the slickbox as soon as the DOM is ready
+	// (a little sooner than page load)
+	$('#hiddenboxf4').hide();
+	// toggles the slickbox on clicking the noted link
+	$('a#jqtextcallf4').click(function() {
+		$('#hiddenboxf4').slideToggle(400);
+		return false;
+	});
+});
+$(document).ready(function() {
+	// hides the slickbox as soon as the DOM is ready
+	// (a little sooner than page load)
+	$('#hiddenboxf5').hide();
+	// toggles the slickbox on clicking the noted link
+	$('a#jqtextcallf5').click(function() {
+		$('#hiddenboxf5').slideToggle(400);
+		return false;
+	});
+});
+$(document).ready(function() {
+	// hides the slickbox as soon as the DOM is ready
+	// (a little sooner than page load)
+	$('#hiddenboxt1').hide();
+	// toggles the slickbox on clicking the noted link
+	$('a#jqtextcallt1').click(function() {
+		$('#hiddenboxt1').slideToggle(400);
+		return false;
+	});
+});
+$(document).ready(function() {
+	// hides the slickbox as soon as the DOM is ready
+	// (a little sooner than page load)
+	$('#hiddenboxt2').hide();
+	// toggles the slickbox on clicking the noted link
+	$('a#jqtextcallt2').click(function() {
+		$('#hiddenboxt2').slideToggle(400);
+		return false;
+	});
+});
+$(document).ready(function() {
+	// hides the slickbox as soon as the DOM is ready
+	// (a little sooner than page load)
+	$('#hiddenboxt3').hide();
+	// toggles the slickbox on clicking the noted link
+	$('a#jqtextcallt3').click(function() {
+		$('#hiddenboxt3').slideToggle(400);
+		return false;
+	});
+});
+$(document).ready(function() {
+	// hides the slickbox as soon as the DOM is ready
+	// (a little sooner than page load)
+	$('#hiddenboxt4').hide();
+	// toggles the slickbox on clicking the noted link
+	$('a#jqtextcallt4').click(function() {
+		$('#hiddenboxt4').slideToggle(400);
+		return false;
+	});
+});
+$(document).ready(function() {
+	// hides the slickbox as soon as the DOM is ready
+	// (a little sooner than page load)
+	$('#hiddenboxt5').hide();
+	// toggles the slickbox on clicking the noted link
+	$('a#jqtextcallt5').click(function() {
+		$('#hiddenboxt5').slideToggle(400);
+		return false;
+	});
+});
+
+/*change end*/
